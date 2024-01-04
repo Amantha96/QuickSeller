56dcef7550876b1544354b6ed298a9d668c2aa27
Amantha96 <amanthabandara1996@gmail.com>
2024-01-04 19:24:00 +0530

    Cpmpleted Header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <h1 className="text-red-500">App</h1>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/Sign-Up" element={<SignUp />}/>
+      <Route path="/Profile" element={<Profile />}/>
+      <Route path="/about" element={<About />}/>
+      
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
